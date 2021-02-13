4487c9af5565413bb1d1dc85d86bffd7e60271fc
gizem-ucar <77545358+gizem-ucar@users.noreply.github.com>
2021-02-13 21:17:31 +0300

    13.02

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface IEntityRepository<T>
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);   //
+        T Get(Expression<Func   <T, bool>> filter);
+        void Add(T entity);
+        void UpDate(T entity);
+        void Delete(T entity);
+
+        List<T> GetAllBycategory(int categoryId);
+    }
+}
